7106418a3d8b11e9faf5d1bb893c97e4d50014cf
sean <heavydragon99@outlook.com>
2026-01-11 16:52:34 +0100

    Added code and cmake to make a basic engine library

---
+++ code/inc/monkeyBrainEngine.h
@@ -0,0 +1,10 @@
+class MonkeyBrainEngine {
+public:
+  MonkeyBrainEngine();
+  ~MonkeyBrainEngine();
+
+public:
+  void initialize();
+  void shutdown();
+  void run();
+};
---
+++ code/src/monkeyBrainEngine.cpp
@@ -0,0 +1,23 @@
+#include "monkeyBrainEngine.h"
+#include <iostream>
+
+MonkeyBrainEngine::MonkeyBrainEngine() {
+  // Constructor implementation
+}
+
+MonkeyBrainEngine::~MonkeyBrainEngine() {
+  // Destructor implementation
+}
+
+void MonkeyBrainEngine::initialize() {
+  // Initialization code
+}
+
+void MonkeyBrainEngine::shutdown() {
+  // Shutdown code
+}
+
+void MonkeyBrainEngine::run() {
+  // Main execution loop
+  std::cout << "MonkeyBrainEngine is running!" << std::endl;
+}
