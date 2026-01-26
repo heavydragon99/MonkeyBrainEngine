ef84cdee751ad36651edad58f8b3595a073168ed
sean <heavydragon99@outlook.com>
2026-01-26 14:08:53 +0100

    Removed debug message from physics system

--- code/systems/physics/src/physicsSystem.cpp
+++ code/systems/physics/src/physicsSystem.cpp
@@ -13,7 +13,5 @@ void PhysicsSystem::update(Registry &aRegistry, float aDt) {
   aRegistry.forEach<Transform, Velocity>(
       [aDt](Transform &aTransform, Velocity &aVelocity) {
         aTransform.mPosition += aVelocity.mSpeed * aDt;
-        std::cout << "Updated entity to position (" << aTransform.mPosition.mX
-                  << ", " << aTransform.mPosition.mY << ")\n";
       });
 }
